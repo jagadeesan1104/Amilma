80b8494742ef13f0855947f7aa5064740f8ffce1
jagadeesan1104 <jagadeesan1104@gmail.com>
2023-12-31 17:34:32 +0530

    api:sales and purchase invoice against customer data

--- amilma_custom/amilma_custom/doctype/amilma_target_setting/amilma_target_setting.js
+++ amilma_custom/amilma_custom/doctype/amilma_target_setting/amilma_target_setting.js
@@ -2,7 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Amilma Target Setting', {
-	// refresh: function(frm) {
-
-	// }
+	target_to(frm){
+		if(frm.doc.target_from){
+			if(frm.doc.target_to){
+				frappe.call({
+					method:"amilma_custom.amilma_custom.doctype.amilma_target_setting.amilma_target_setting.get_month",
+					args:{
+						from_date:frm.doc.target_from,
+					},
+					callback(r){
+						if(r.message){
+							frm.set_value('month',r.message)
+						}
+					}
+				})	
+			}
+		}
+		else{
+			frappe.throw(__("Please Select from date and then select the to date"));
+		}
+	}
 });
--- amilma_custom/amilma_custom/doctype/amilma_target_setting/amilma_target_setting.py
+++ amilma_custom/amilma_custom/doctype/amilma_target_setting/amilma_target_setting.py
@@ -1,8 +1,19 @@
 # Copyright (c) 2023, Vivek and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from datetime import datetime
+
+from frappe.utils import now,getdate,today,format_date,nowdate,add_months
+
 
 class AmilmaTargetSetting(Document):
-	pass
+    pass
+
+
+
+@frappe.whitelist()
+def get_month(from_date):
+    start_date = datetime.strptime(from_date, '%Y-%m-%d')
+    return start_date.month
