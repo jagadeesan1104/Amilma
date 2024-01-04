24dc08468b02bc2107b7b731ae9263578fd12f0d
jagadeesan1104 <jagadeesan1104@gmail.com>
2024-01-04 11:22:31 +0530

    bug-fix:melting cliam validation

--- amilma_custom/amilma_custom/doctype/melting_claim/melting_claim.py
+++ amilma_custom/amilma_custom/doctype/melting_claim/melting_claim.py
@@ -8,22 +8,24 @@ from frappe import _
 
 class MeltingClaim(Document):
 	def validate(self):
-		over_all_total_qty = 0
+		over_all_total_qty = 0.0
 		over_all_total_amount = 0
 		for claim in self.melting_claim_items:
 			get_uom_and_pieces = frappe.db.get_value('Item',{'name':claim.item},['stock_uom','no_of_pieces'])
 			if get_uom_and_pieces[0] == "Box":
 				get_item_price = frappe.db.get_value('Item Price',{'item_code':claim.item,'price_list':"Distributor Price",'selling':1},['price_list_rate'])
 				if get_item_price:
-					get_rate = get_item_price / float(get_uom_and_pieces[1])
-					claim.rate = get_rate
-					claim.amount = claim.rate * claim.req_qty
-
-
+					if get_uom_and_pieces[1]:
+						get_rate = get_item_price / float(get_uom_and_pieces[1])
+						claim.rate = get_rate
+						claim.amount = float(claim.rate) * float(claim.req_qty)
+					else:
+						frappe.throw(_('In Item No.of.Pieces Not Entered it is empty so not calculated the rate'))	
+					
 			over_all_total_amount += claim.amount
 			self.net_total = over_all_total_amount
 
-			over_all_total_qty += claim.req_qty
+			over_all_total_qty += float(claim.req_qty)
 			self.total_qty = over_all_total_qty
 
 
@@ -33,9 +35,12 @@ def get_item_rate(item):
 	if get_uom_and_pieces[0] == "Box":
 		get_item_price = frappe.db.get_value('Item Price',{'item_code':item,'price_list':"Distributor Price",'selling':1},['price_list_rate'])
 		if get_item_price:
-			get_rate = get_item_price / float(get_uom_and_pieces[1])
+			if get_uom_and_pieces[1]:
+				get_rate = get_item_price / float(get_uom_and_pieces[1])
+			else:
+				frappe.throw(_('In Item No.of.Pieces Not Entered it is empty so not calculated the rate'))	
 		else:
-			frappe.throw(_('Please Check the Item Price List in price list '))	
+			frappe.throw(_('Item Price, Price List only in Distributor Price'))	
 		return get_rate
 	else:
 		frappe.throw(_('Item UOM Not set Box Type Please Set Box Type UOM'))	
