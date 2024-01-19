6eb5c380651ce244468f0fe898a2e33d4637732d
jagadeesan1104 <jagadeesan1104@gmail.com>
2024-01-19 12:11:24 +0530

    report:purchase and sales invoice

--- amilma_custom/amilma_custom/report/purchase_gst_filing/purchase_gst_filing.py
+++ amilma_custom/amilma_custom/report/purchase_gst_filing/purchase_gst_filing.py
@@ -37,9 +37,9 @@ def get_columns(filters):
 def get_data(filters):
 	data = []
 	if filters.company:
-		get_purchase_invoice = frappe.db.get_all("Purchase Invoice",{'docstatus':1,'posting_date':('between',(filters.from_date,filters.to_date)),'company':filters.company},['supplier_name','tax_id','posting_date','name','base_net_total','rounded_total'])
+		get_purchase_invoice = frappe.db.get_all("Purchase Invoice",{'docstatus':1,'posting_date':('between',(filters.from_date,filters.to_date)),'company':filters.company},['supplier_name','tax_id','posting_date','name','base_net_total','rounded_total','status'])
 	else:
-		get_purchase_invoice = frappe.db.get_all("Purchase Invoice",{'docstatus':1,'posting_date':('between',(filters.from_date,filters.to_date))},['supplier_name','tax_id','posting_date','name','base_net_total','rounded_total'])
+		get_purchase_invoice = frappe.db.get_all("Purchase Invoice",{'docstatus':1,'posting_date':('between',(filters.from_date,filters.to_date))},['supplier_name','tax_id','posting_date','name','base_net_total','rounded_total','status'])
 	for purchase in get_purchase_invoice:
 			sgst_tax_amount = get_sgst_tax_amount(purchase.name)
 			cgst_tax_amount = get_cgst_tax_amount(purchase.name)
